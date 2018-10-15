52f13f5ac0bd817d09f43c5dfc9d08846afbfc79
ClementsJR <jrclements@ualr.edu>
2018-10-15 17:25:51 -0500

    First 5 levels are now generated as cave levels

--- src/engine/Dungeon.java
+++ src/engine/Dungeon.java
@@ -8,17 +8,20 @@ public class Dungeon {
 	public static final int DEFAULT_NUM_COLS = 32;
 	
 	private static final int MAX_NUM_FLOORS = 30;
-	
+
+	private Range cellularAutomataLevels;
 	private ArrayList<Floor> floors;
 	private int currentFloorIndex;
 	
 	public Dungeon() {
+		cellularAutomataLevels = new Range(0, 4);
+		
 		floors = new ArrayList<Floor>();
 		
 		Random rand = new Random();
 		long seed = rand.nextLong();
 		
-		generateNewFloor(seed, Floor.MapGenAlgorithm.BSP, true, false);
+		generateNewFloor(seed, Floor.MapGenAlgorithm.CELLULAR_AUTOMATA, true, false);
 		
 		currentFloorIndex = 0;
 	}
@@ -45,7 +48,26 @@ public class Dungeon {
 		if (currentFloorIndex == floors.size()) {
 			Random rand = new Random();
 			long seed = rand.nextLong();
-			generateNewFloor(seed, Floor.MapGenAlgorithm.BSP, false, currentFloorIndex == MAX_NUM_FLOORS);
+			
+			Floor.MapGenAlgorithm algorithm = (cellularAutomataLevels.isInRange(currentFloorIndex) ? Floor.MapGenAlgorithm.CELLULAR_AUTOMATA : Floor.MapGenAlgorithm.BSP);
+			
+			generateNewFloor(seed, algorithm, false, currentFloorIndex == MAX_NUM_FLOORS);
+		}
+	}
+
+	protected class Range {
+		private int lowerBound;
+		private int upperBound;
+		
+		public Range(int lowerBound, int upperBound) {
+			this.lowerBound = lowerBound;
+			this.upperBound = upperBound;
+		}
+		
+		public boolean isInRange(int num) {
+			boolean isInRange;
+			isInRange = (lowerBound <= num && num <= upperBound);
+			return isInRange;
 		}
 	}
 }
