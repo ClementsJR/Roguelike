f43d43c5647fda0f6e8259add8aba70045d39f27
Taylor Kyzer <tkyzer126@gmail.com>
2018-11-18 16:30:05 -0600

    Pick up armor

--- src/engine/PlayerCharacter.java
+++ src/engine/PlayerCharacter.java
@@ -1,5 +1,7 @@
 package engine;
 
+import java.util.ArrayList;
+
 public abstract class PlayerCharacter extends LivingEntity{
 	
 	public static int PLAYER_LEVEL = 0;
@@ -8,12 +10,14 @@ public abstract class PlayerCharacter extends LivingEntity{
 	public boolean hasFood = false;
 	private int goalXP = (int)Math.pow(2, (3 + PLAYER_LEVEL));
 	private Food playersFood;
+	private ArrayList <Armor> armorList;
 	
 	private double hungerLevel;
 
 	public PlayerCharacter(int maxHealth, Range initAttackRange, int defense) {
 		super(maxHealth, initAttackRange, defense);
 		setIsEnemy(false);
+		armorList = new ArrayList <Armor>();
 	}
 	
 	public enum HungerStage {
@@ -38,6 +42,10 @@ public abstract class PlayerCharacter extends LivingEntity{
 			playersFood = null;
 			hasFood = false;
 		}
+	}
+	
+	public void GiveArmor(Armor newArmor) {
+		armorList.add(newArmor);
 	}
 	
 	public abstract void LevelUp();
